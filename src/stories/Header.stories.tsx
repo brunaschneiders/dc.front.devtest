6f1c5004a671e9af7386c6f699fb1859d0719804
Bruna Schneiders <bruna.schneiders@gmail.com>
2021-08-20 16:55:40 -0300

    feat: create header story

---
+++ src/stories/Header.stories.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Search as SearchIcon } from '@material-ui/icons';
+
+import { Header, Input } from '../components';
+
+import { theme } from '../styles/theme';
+
+export default {
+  title: 'Header',
+  component: Header,
+  argTypes: {
+    rightContent: {
+      options: ['Null', 'Input'],
+      mapping: {
+        Null: null,
+        Input: (
+          <Input
+            type='text'
+            name='search'
+            ariaLabel='Buscar usuários'
+            placeholder='Buscar usuários'
+            icon={<SearchIcon color='primary' />}
+          />
+        )
+      }
+    }
+  },
+  args: {
+    title: 'Title'
+  },
+  parameters: {
+    backgrounds: {
+      default: 'primary',
+      values: [
+        {
+          name: 'primary',
+          value: theme.palette.primary.main
+        }
+      ]
+    }
+  }
+} as ComponentMeta<typeof Header>;
+
+const Template: ComponentStory<typeof Header> = (args) => <Header {...args} />;
+
+export const Default = Template.bind({});
+
+export const WithRightContent = Template.bind({});
+WithRightContent.args = {
+  rightContent: 'Input'
+};
