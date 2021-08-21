fe5cdce9f516a34a1b9b66cc7bbd2e4aa398125d
Bruna Schneiders <bruna.schneiders@gmail.com>
2021-08-21 20:40:11 -0300

    fix (table): adjust padding

--- src/components/tables/SimpleTable/TableHeader/styles.ts
+++ src/components/tables/SimpleTable/TableHeader/styles.ts
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 export const useStyles = makeStyles((theme) => ({
   row: {
     '&& th': {
-      padding: theme.spacing(1.5),
+      padding: `0 ${theme.spacing(2.5)}px`,
       borderBottom: `2px solid ${theme.palette.primary.main}`,
       [theme.breakpoints.down('sm')]: {
         padding: `0 ${theme.spacing(0.5)}px`
